850f239ff462a6fea9de7fc78e310b6f0a59c9e9
Aleksei Guliaev <guliaevaleksei@gmail.com>
2021-11-12 13:56:22 +0100

    Update dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,28 +51,28 @@ android {
 }
 
 dependencies {
-    implementation("com.google.firebase:firebase-crashlytics-ktx:18.2.3")
-    implementation("com.google.firebase:firebase-analytics-ktx:19.0.2")
+    implementation("com.google.firebase:firebase-crashlytics-ktx:18.2.4")
+    implementation("com.google.firebase:firebase-analytics-ktx:20.0.0")
 
-    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:1.1.0-alpha06")
-    implementation("androidx.compose.material:material:1.1.0-alpha06")
-    implementation("androidx.compose.ui:ui-tooling:1.1.0-alpha06")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.4.0-rc01")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
+    implementation("androidx.compose.ui:ui:1.1.0-beta02")
+    implementation("androidx.compose.material:material:1.1.0-beta02")
+    implementation("androidx.compose.ui:ui-tooling:1.1.0-beta02")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.navigation:navigation-compose:2.4.0-beta02")
     implementation("androidx.webkit:webkit:1.4.0")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-alpha06")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-beta02")
 
-    implementation("androidx.compose.material:material-icons-core:1.1.0-alpha06")
-    implementation("androidx.compose.material:material-icons-extended:1.1.0-alpha06")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-rc01")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
+    implementation("androidx.compose.material:material-icons-core:1.1.0-beta02")
+    implementation("androidx.compose.material:material-icons-extended:1.1.0-beta02")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+    implementation("androidx.navigation:navigation-compose:2.4.0-beta02")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01")
 
     //Accompanist
@@ -101,12 +101,12 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
     // Coroutine Lifecycle Scopes
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
 
     // LiveData
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0-rc01")
-    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-alpha06")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-beta02")
 
     //Dagger - Hilt
     implementation("com.google.dagger:hilt-android:2.38.1")
@@ -124,5 +124,5 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.7.0")
 
     //Custom tabs
-    implementation("androidx.browser:browser:1.3.0")
+    implementation("androidx.browser:browser:1.4.0")
 }
